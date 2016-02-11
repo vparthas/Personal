b079c70c1546c2f81f572d1ee9b5370f607437bb
vparthas <vprthsr2@illinois.edu>
2016-02-11 04:18:28 -0600

    new gitignore

--- Potfolio_New/js/scroll.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('#aboutLink').click(function()
-    {
-        $('html, body').animate({
-            scrollTop: $("#aboutDiv").offset().top// - $('.splash-container').height()
-        }, 1000);
-    });
-});
--- README.md
+++
@@ -1,2 +0,0 @@
-# Portfolio Website
-Project files for varunp.com
